afa3cc45b403a99a52876f4ada02024ba374928d
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 03:16:49 +0500

    completed task 13

---
+++ Your_Own_Array(q-13)/transportationMode.js
@@ -0,0 +1,9 @@
+const fvrtTransportationMode = [
+  'I would like to own grey toyota Land cruiser V8',
+  'The land cruiser is powered by a 5.7 liter v-8 engine speed automatic transmission.',
+  'The land cruiser delivers comfortable driving dynamics',
+]
+
+fvrtTransportationMode.map((fvrtMode) => {
+  console.log(fvrtMode)
+})
