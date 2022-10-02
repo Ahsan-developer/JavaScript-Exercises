0839420bd5a37ea053a287a1d8fe09bea1d3a0ab
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 03:36:12 +0500

    completed task : guest list invitation

---
+++ GuestList(q-14)/guestList.js
@@ -0,0 +1,7 @@
+const invitationMessage =
+  "Sending you my heartiest invitation to join this dinner party. your presence is all that's needed to make this party a memorable one oct 12,2022 @8 o clock."
+const friends = ['Noman', 'Arslan', 'Kamran']
+
+friends.map((friend) => {
+  console.log(`Hi ${friend}: \n ${invitationMessage}\n`)
+})
