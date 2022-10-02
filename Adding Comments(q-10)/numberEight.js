6748ac1b7500c7173c1d022517042fdfbf1ceb9e
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 02:34:59 +0500

    completed task : Adding Comments in a program.

---
+++ Adding Comments(q-10)/numberEight.js
@@ -0,0 +1,7 @@
+// Muhammad Ahsan Riaz Monday, 3 Octuber 2022
+// its a program for apply basic math functions (add, subtraction,multiplication,division)
+
+console.log('Addition Operation: 4 + 4 =', 4 + 4)
+console.log('Subtraction Operation 9 - 1 =', 9 - 1)
+console.log('Multiplication Operation 2 * 4 =', 2 * 4)
+console.log('Division Operation 16 / 2', 16 / 2)
---
+++ Adding Comments(q-10)/strippingNames.js
@@ -0,0 +1,5 @@
+// Muhammad Ahsan Riaz Monday, 3 Octuber 2022
+// its a program for escape Character used with string and white space trim from beginning and end.
+const personName = '\n\tMuhammad Ahsan Riaz\t\n'
+console.log(personName)
+console.log(personName.trim(), '\n')
