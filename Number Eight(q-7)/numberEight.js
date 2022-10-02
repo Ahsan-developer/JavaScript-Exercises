c0267fc7ae43b4858634f3e53d4ba8719ed4a179
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 01:38:09 +0500

    copmpleted Number Eight

---
+++ Number Eight(q-7)/numberEight.js
@@ -0,0 +1,4 @@
+console.log('Addition Operation: 4 + 4 =', 4 + 4)
+console.log('Subtraction Operation 9 - 1 =', 9 - 1)
+console.log('Multiplication Operation 2 * 4 =', 2 * 4)
+console.log('Division Operation 16 / 2', 16 / 2)
