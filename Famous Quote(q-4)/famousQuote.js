009a7da9a0493118a19176c5cfa81b963d4f1645
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 00:50:19 +0500

    completed task famous Quote

---
+++ Famous Quote(q-4)/famousQuote.js
@@ -0,0 +1,5 @@
+console.log(
+  `Hazrat Muhammad SAW said “I leave behind me two things, The Quran and My Sunnah and if you follow these you will never go astray.”`,
+)
+
+//  astray meaning is "away from the correct path or direction"
