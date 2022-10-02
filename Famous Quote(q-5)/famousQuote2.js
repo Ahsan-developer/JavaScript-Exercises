6a4683ac810ec5d05199e50c70ced509d415e649
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 01:00:45 +0500

    completed famousQuote2

---
+++ Famous Quote(q-5)/famousQuote2.js
@@ -0,0 +1,3 @@
+const famous_person = 'Hazrat Muhammad SAW'
+const message = `${famous_person} said “I leave behind me two things, The Quran and My Sunnah and if you follow these you will never go astray.\nastray meaning is "away from the correct path or direction"”`
+console.log(message)
