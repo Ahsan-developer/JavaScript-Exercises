f9a66a8be58b7073e2b99d20743499a5c96f4fc1
Ahsan-developer <44539525+Ahsan-developer@users.noreply.github.com>
2022-10-03 02:48:03 +0500

    completed task

---
+++ NamesList(q-11)/nameList.js
@@ -0,0 +1,12 @@
+let friendsName = [
+  'Noman',
+  'Umer Idress',
+  'Muhammad Javed',
+  'Malik Shahid',
+  'Mujeeb Tahir',
+  'Umair Saeed',
+]
+
+friendsName.map((name, index) => {
+  console.log(name)
+})
